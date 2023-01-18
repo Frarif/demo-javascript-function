594e886b2c98d4c0ffd1ed4c48da90c2b5154107
Frarif <freakinarif25@gmail.com>
2023-01-18 19:19:17 +0700

    Refix: Javascript

--- triangle/index.js
+++ triangle/index.js
@@ -1,11 +1,11 @@
-// const inputValue = confirm(`Are you sure wanna input value ?`);
+const inputValue = confirm(`Are you sure wanna input value ?`);
 
-// if (inputValue) {
-//   alert(`Please input the value`);
-// } else {
-//   alert(`OK, see you soon.`);
-//   areaTriangle.remove();
-// }
+if (inputValue) {
+  alert(`Please input the value`);
+} else {
+  alert(`OK, see you soon.`);
+  areaTriangle.remove();
+}
 
 // Input from user
 const base = parseInt(prompt(`Enter base value of the triangle (m): `));
