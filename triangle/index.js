ffc89ef6b33881dfd8a57cdd4e527e7e70a419b4
M Haidar Hanif <git@mhaidarhanif.com>
2023-01-18 21:12:03 +0700

    feat: improve code

--- triangle/index.js
+++ triangle/index.js
@@ -35,55 +35,54 @@ function runApp() {
 
 runApp();
 
-// Array
-// const areaDataUnits = [
-//   { name: "km²", multiplied: 1 / 1000000 },
-//   { name: "hm²", multiplied: 1 / 10000 },
-//   { name: "dam²", multiplied: 1 / 100 },
-//   { name: "dm²", multiplied: 100 },
-//   { name: "cm²", multiplied: 10000 },
-//   { name: "mm²", multiplied: 1000000 },
-// ];
+const areaDataUnits = [
+  { name: "km²", multiplier: 1 / 1000000 },
+  { name: "hm²", multiplier: 1 / 10000 },
+  { name: "dam²", multiplier: 1 / 100 },
+  { name: "dm²", multiplier: 100 },
+  { name: "cm²", multiplier: 10000 },
+  { name: "mm²", multiplier: 1000000 },
+];
 
-// const perimeterDataUnits = [
-//   { name: "km", multiplied: 1 / 1000 },
-//   { name: "hm", multiplied: 1 / 100 },
-//   { name: "dam", multiplied: 1 / 10 },
-//   { name: "dm", multiplied: 10 },
-//   { name: "cm", multiplied: 100 },
-//   { name: "mm", multiplied: 1000 },
-// ];
+const perimeterDataUnits = [
+  { name: "km", multiplier: 1 / 1000 },
+  { name: "hm", multiplier: 1 / 100 },
+  { name: "dam", multiplier: 1 / 10 },
+  { name: "dm", multiplier: 10 },
+  { name: "cm", multiplier: 100 },
+  { name: "mm", multiplier: 1000 },
+];
 
-// const areaDataConverted = [];
-// const perimeterDataConverted = [];
+const dataAreaConverted = [];
+const dataPerimeterConverted = [];
 
-// // Loopings
-// for (let index = 0; index < areaDataUnits.length; index++) {
-//   const areaConvert = areaDataUnits[index];
-//   const areaConverted = {
-//     name: `${areaConvert.name}`,
-//     areaValue: areaResult * areaConvert.multiplied,
-//   };
-//   areaDataConverted[index] = areaConverted;
-// }
+// Loopings
+for (let index = 0; index < areaDataUnits.length; index++) {
+  const areaConvert = areaDataUnits[index];
+  const areaConverted = {
+    name: `${areaConvert.name}`,
+    areaValue: areaResult * areaConvert.multiplier,
+  };
+  dataAreaConverted[index] = areaConverted;
+}
 
-// for (let index = 0; index < perimeterDataUnits.length; index++) {
-//   const perimeterConvert = perimeterDataUnits[index];
-//   const perimeterConverted = {
-//     name: `${perimeterConvert.name}`,
-//     perimeterValue: perimeterResult * perimeterConvert.multiplied,
-//   };
-//   perimeterDataConverted[index] = perimeterConverted;
-// }
+for (let index = 0; index < perimeterDataUnits.length; index++) {
+  const perimeterConvert = perimeterDataUnits[index];
+  const perimeterConverted = {
+    name: `${perimeterConvert.name}`,
+    perimeterValue: perimeterResult * perimeterConvert.multiplier,
+  };
+  dataPerimeterConverted[index] = perimeterConverted;
+}
 
-// // Display the result
-// for (let index = 0; index < areaDataConverted.length; index++) {
-//   const areaDataValue = areaDataConverted[index];
-//   const perimeterDataValue = perimeterDataConverted[index];
-//   console.log(
-//     `The area ${areaResult} m² is equal to ${areaDataValue.areaValue} ${areaDataValue.name} `
-//   );
-//   console.log(
-//     `The perimeter ${perimeterResult} m is equal to ${perimeterDataValue.perimeterValue} ${perimeterDataValue.name}`
-//   );
-// }
+// Display the result
+for (let index = 0; index < dataAreaConverted.length; index++) {
+  const areaDataValue = dataAreaConverted[index];
+  const perimeterDataValue = dataPerimeterConverted[index];
+  console.log(
+    `The area ${areaResult} m² is equal to ${areaDataValue.areaValue} ${areaDataValue.name} `
+  );
+  console.log(
+    `The perimeter ${perimeterResult} m is equal to ${perimeterDataValue.perimeterValue} ${perimeterDataValue.name}`
+  );
+}
